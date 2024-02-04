5a876f36df22b72d72d40aef90377a924c6727b3
ImRaabe <raabees@mail.uc.edu>
2024-02-04 02:29:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double winterCost = 2000;
+        double springCost = 1000;
+        double summerCost = 1500;
+        double fallCost = 1000;
+        double yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Winter Cost: $" + winterCost);
+        System.out.println("Spring Cost: $" + springCost);
+        System.out.println("Summer Cost: $" + summerCost);
+        System.out.println("Fall Cost: $" + fallCost);
+        System.out.println("Total Yearly Cost: $" + yearlyCost);
+
+        }
+    }
